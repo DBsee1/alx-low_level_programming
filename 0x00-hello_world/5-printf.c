f064f743d8d690fdcddbf6ee65f58cffca4292e7
DBsee1 <dominic.bassee.xf@gmail.com>
2022-12-08 07:13:39 -0800

    using printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main	-	Entry point
+ * Return:	Always	Return	0	(success)
+ */
+int	main(void)
+{
+	printf("with	a	proper	grammar	,	but	the	outcome	is	a	pieces	of	art,\n");
+	return	(0);
+}
