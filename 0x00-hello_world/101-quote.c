10fa3ccb08711bc41710617c77dfe58c6d6061b1
DBsee1 <dominic.bassee.xf@gmail.com>
2022-12-08 20:43:17 -0800

    using wall gcc

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ *main -	Entry	point	of	our	code;
+ *
+ *Description:	Print	a	stiring	to	a	std	err;
+ *
+ *
+ *Return:	always	1	(err);
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	return (1);
+}
