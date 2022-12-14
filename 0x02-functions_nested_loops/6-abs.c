b21db0cc67f20e58b62dd0d367d4f67e51328ef7
DBsee1 <dominic.bassee.xf@gmail.com>
2022-12-14 02:08:35 -0800

    absolute number

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,14 +2,14 @@
 
 /**
  *_abs	-	 a function that computes the absolute value of an integer;
- *@int:	a data type;
+ *@n:	a data type;
  * Return:	Always return (0);
  */
 
-int _abs(int)
+int _abs(int n)
 {
-	if (int < 0)
-		return (-int);
+	if (n < 0)
+		return (-n);
 	else
-		return (int);
+		return (n);
 }
