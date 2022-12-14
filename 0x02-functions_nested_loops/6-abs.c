54f58b65057befef1549e1e9e78f2ee2b371eec1
DBsee1 <dominic.bassee.xf@gmail.com>
2022-12-14 02:05:58 -0800

    cmpute an absolute number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *_abs	-	 a function that computes the absolute value of an integer;
+ *@int:	a data type;
+ * Return:	Always return (0);
+ */
+
+int _abs(int)
+{
+	if (int < 0)
+		return (-int);
+	else
+		return (int);
+}
