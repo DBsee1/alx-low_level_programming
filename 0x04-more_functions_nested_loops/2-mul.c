05104eb9b831c55856c96dbe0c9b5c1c7b13bf9f
DBsee1 <dominic.bassee.xf@gmail.com>
2022-12-16 04:34:11 -0800

    mandatory tasks:betty style:pass & locally tested

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ *mul	-	a function that multiplies two integers;
+ *
+ *@a:	takes an int and multiple with @b;
+ *@b:	takes an input and multiply with @a;
+ *Return: return total_mul;
+ *
+ */
+
+int mul(int a, int b)
+{
+	int total_mul;
+
+	total_mul = (a * b);
+	return (total_mul);
+}
