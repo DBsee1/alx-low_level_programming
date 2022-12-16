6fbd6126fb5be7428e0db47ff82c864b14df145b
DBsee1 <dominic.bassee.xf@gmail.com>
2022-12-15 23:58:11 -0800

    mandatory tasks -betty test:pass & locally tested

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *_isupper	-	Write a function that checks for uppercase character;
+ *
+ *@c:	Takes a char input;
+ *Returns 1 if c is uppercase;
+ *Returns 0 otherwise;
+ *
+ * Return:	Return (0) if successfull;
+ */
+
+int _isupper(int c)
+{
+	/*Using if-statement*/
+
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+
+}
