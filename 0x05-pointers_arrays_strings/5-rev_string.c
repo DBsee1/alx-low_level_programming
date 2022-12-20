2208f1346648b78d5cb1aa5dd97aee7f2805f656
DBsee1 <dominic.bassee.xf@gmail.com>
2022-12-20 04:58:35 -0800

    testing phase

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,13 +1,20 @@
 #include "main.h"
 
+/**
+ * rev_string	-	function that prints a string, in reverse,
+ * followed by a new line;
+ * @s:	takes input (string);
+ *
+ * Return:	Return a string in reverse;
+ */
+
 void rev_string(char *s)
 {
 	int i;
-	int count =0;
+	int count = 0;
 	char *save_original_value_of_s = s;
 
-	/* calculate the length of s */
-	while(*s != '\0')
+	while (*s != '\0')
 	{
 		count++;
 		s++;
