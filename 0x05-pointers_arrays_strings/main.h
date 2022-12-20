a427bc9435ae8501fb3129e4506576d5edcef013
DBsee1 <dominic.bassee.xf@gmail.com>
2022-12-19 21:43:15 -0800

    tested locall and Betty style docs pass

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char c);
 void reset_to_98(int *n);
@@ -13,3 +13,4 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 
+#endif /*_MAIN_H */
