db69e98ad648dd37c2021a4b9631472ddaa52a9e
DBsee1 <dominic.bassee.xf@gmail.com>
2022-12-19 21:24:03 -0800

    Tested locally and betty style docs pass

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *reset_to_98	-	a function that takes a pointer to an int as parameter and
+ *update the value it points to to 98
+ *
+ *@n:	pointing to n variable;
+ *
+ * Return:	void;
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
