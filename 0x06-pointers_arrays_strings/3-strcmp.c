b7c88770b661d0ecd5f85ea8de98cd4127984ef2
DBsee1 <117823925+DBsee1@users.noreply.github.com>
2022-12-23 18:53:30 +0100

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,25 +1,19 @@
 #include "main.h"
 
 /**
- * _strcmp	-	 a function that compares two strings.
- * @s1:	comparing string input
- * @s2: comparing string lexicograph
- * Return: return an int;
+ * _strcmp - a function that compares two strings
+ * @s1: input one
+ * @s2: input two
+ * Return: Always 0 (Success)
  */
 int _strcmp(char *s1, char *s2)
 {
-	int ressult = 0;
+	int i;
 
-	while (result == 0 && *s1 != '\0' && *s2 != '\0')
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
-		result = *s1 - *s2;
-		s1++;
-		s2++;
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
 	}
-
-	if (result == 0)
-	{
-		result = *s1 - *s2;
-	}
-	return (result);
+	return (0);
 }
