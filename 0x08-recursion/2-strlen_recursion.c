2013627afc5d4091d9d8d6aebe6b602af2358635
DBsee1 <dominic.bassee.xf@gmail.com>
2023-01-04 10:47:01 -0800

    tested locally return (0)

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *_strlen_recursion	-	a function tht print the lenght of a string
+ *@s:	take in a string as an input
+ *Return:	Always return (0) successs:
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	(return 1) + _strlen_recursion(s + 1);
+}
