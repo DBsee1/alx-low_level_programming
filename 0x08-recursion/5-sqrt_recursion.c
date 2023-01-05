7ec35c94a86db3cf6c430a9db05db055084c4b22
DBsee1 <dominic.bassee.xf@gmail.com>
2023-01-04 20:39:22 -0800

    bug fix

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -13,9 +13,9 @@ int _sqrt_recursion(int n)
 	if (n < 0)
 		return (-1);
 	if ((n == 0) || (n == 1))
-		return (1);
+		return;
 
-	for (i = 1; i * i <= n; i++)
+	for (i = 2; i <= n/2; i++)
 	{
 		if (i * i == n)
 			return (i);
