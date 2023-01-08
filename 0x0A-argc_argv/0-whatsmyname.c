4785b995da0dbafa4deb17df476cf978d45d082c
DBsee1 <dominic.bassee.xf@gmail.com>
2023-01-08 06:38:38 -0800

    adding description to the main func

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,13 +2,16 @@
 #include "main.h"
 
 /**
- * Write a program that prints its name, followed by a new line.
- * main	-	Code Entry 
+ *main	-	 Write a program that prints its name, followed by a new line.
+ * @argc:	the number of argument pass in the command line
+ * @argv:	array of string pointing to char
  * Return:	Always Return (0) success
  */
 
 int main(int argc, char *argv[])
 {
+	argc = 0;
+
 	printf("%s\n", argv[0]);
-	return(0);
+	return (0);
 }
