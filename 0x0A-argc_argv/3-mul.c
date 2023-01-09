1a8a1d01b3e708cf89fd0979113bc77b325a939b
DBsee1 <dominic.bassee.xf@gmail.com>
2023-01-08 20:56:16 -0800

    refactor and updated

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,13 +10,13 @@
 
 int main(int argc, char *argv[])
 {
-	if (argc != 3)
-	{
-		fprintf(stderr, "Error\n");
-		return (1);
-	}
 	int result;
 
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
 	result = atoi(argv[1]) * atoi(argv[2]);
 	printf("%d\n", result);
 	return (0);
