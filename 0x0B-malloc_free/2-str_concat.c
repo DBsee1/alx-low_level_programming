553f03747728ff1af7ba415f976bd0327cfcdae7
DBsee1 <dominic.bassee.xf@gmail.com>
2023-01-11 15:30:24 -0800

    s1 & s2 bug fix

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,8 +15,10 @@ char *str_concat(char *s1, char *s2)
 	int size;
 	char *arr;
 
-	if (s1 == NULL || s2 == NULL)
-		s1 = s2 = "";
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
 	size = strlen(s1) + strlen(s2) + 1;
 	arr = malloc(sizeof(char) * size);
