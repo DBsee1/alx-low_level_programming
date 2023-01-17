9b5998148f6d074e5e3e5a907367ba3a9e23a5d2
DBsee1 <117823925+DBsee1@users.noreply.github.com>
2023-01-17 07:08:41 +0100

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,12 @@
-#ifndef _DOC_H_
+#ifndef _DOG_H_
 #define _DOG_H_
 
 /**
- * struct dog - structure
- * @name: name
- * @age: age
- * @owner: owner
+ * struct dog - information about dogs
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
  */
-
 struct dog
 {
 	char *name;
@@ -15,11 +14,15 @@ struct dog
 	char *owner;
 };
 
+/**
+ * dog_t - Typedef for struct dog
+ */
 typedef struct dog dog_t;
 
+int _putchar(char);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
-#endif /*DOG_H*/
+#endif /* _DOG_H_ */
